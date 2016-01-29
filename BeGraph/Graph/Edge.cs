5873b255963ada3f6a096c643b799a9104cf6474
Bernatovich Anatoliy <raixur@hotmail.com>
2016-01-29 02:48:42 +0200

    Initialization

---
+++ BeGraph/Graph/Edge.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace BeGraph
+{
+    class Edge:Figure{
+
+		Vertex first;
+		Vertex second;
+        double weight;
+
+        public override string ToString(){
+            return first + "->" + second + " (" + weight + ")";
+        }
+
+        public Edge(Vertex f, Vertex s, double w = 0.0){
+            first = f;
+            second = s;
+            weight = w;
+        }
+
+		public Vertex getFirst() {
+			return first;
+		}
+
+		public Vertex getSecond() {
+			return second;
+		}
+
+		public override void draw(PictureBox pb) {
+			// Висчитавание координат начала и конца вершины
+			double c = Math.Sqrt(Math.Pow(second.position.X - first.position.X, 2) + Math.Pow(second.position.Y - first.position.Y, 2));
+			double sin = (second.position.Y - first.position.Y) / c;
+			double cos = (second.position.X - first.position.X) / c;
+
+
+			Point fRad = new Point(first.position.X + (int)(cos * Vertex.r), first.position.Y + (int)(sin * Vertex.r));
+			Point sRad = new Point(second.position.X - (int)(cos * Vertex.r), second.position.Y - (int)(sin * Vertex.r));
+			Graphics gr = pb.CreateGraphics();
+			Pen p = new Pen(Color.DarkBlue,2);
+
+			// Рисование линии между вершинам графа
+			gr.DrawLine(p, fRad, sRad);
+
+			p.Dispose();
+			gr.Dispose();
+		}
+	}
+}
---
+++ BeGraph/Graph/Figure.cs
@@ -0,0 +1,15 @@
+﻿namespace BeGraph {
+    abstract class Figure {
+        public int id {
+			get; private set;
+		}
+        static int nextID = 0;
+
+        public Figure() {
+            id = nextID++;
+        }
+
+
+		public abstract void draw(System.Windows.Forms.PictureBox pb);
+    }
+}
